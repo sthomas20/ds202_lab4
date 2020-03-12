aa2b0685e4fff3003ee5f063a1e5afd8c7ff08fd
sthomas20 <sthomas8@iastate,edugit config --global user.email sthomas8@iastate.edu>
2020-03-12 14:34:10 -0500

    Updated Q1 and Q2

--- individual1.R
+++ individual1.R
@@ -4,9 +4,38 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 defense <- read_excel('cyclonesFootball2019.xlsx', sheet='Defensive')
-str(defense)
+offense <- read_excel('cyclonesFootball2019.xlsx', sheet='Offensive')
+bio<- read_excel('cyclonesFootball2019.xlsx', sheet='Biography')
+
 
 #PART ONE: Cleaning Data 
 
-#Question 1
+#Question 1 and 2
+defClean<- defense %>% 
+  mutate(Name =as.factor(Name)) %>%
+  mutate(Opponent_Opponent = as.factor(Opponent_Opponent)) %>%
+  mutate_at(vars(contains("Tackles")), funs(as.numeric)) %>%
+  mutate_at(vars(contains("Turnover")), funs(as.numeric)) %>%
+  mutate_at(vars(contains("Pass")), funs(as.numeric)) 
+  
+str(defense)
+
+offClean<- offense %>% 
+  mutate(Name =as.factor(Name)) %>%
+  mutate(Opponent_Opponent = as.factor(Opponent_Opponent)) %>%
+  mutate_at(vars(contains("Rushing")), funs(as.numeric)) %>%
+  mutate_at(vars(contains("Receiving")), funs(as.numeric)) %>%
+  mutate_at(vars(contains("Passing")), funs(as.numeric)) 
+ 
+str(offense)
+
+bioClean<- bio %>% 
+  mutate(Name =as.factor(Name)) %>%
+  mutate_at(vars(contains("Weight")), funs(as.numeric)) %>%
+  separate(Height, into = c("ft", "inches")) %>%
+  mutate(Height = as.numeric(ft)*12 + as.numeric(inches)) 
+  
+str(bioClean)
+
+#Question 3
 

5556ae476652778e868b0dc230d308b5adb9346e
John Nownes <jnownes@iastate.edu>
2020-03-12 15:31:01 -0500

    Finished part 1

--- Individual2.R
+++ Individual2.R
@@ -4,9 +4,24 @@ offense = read_excel('cyclonesFootball2019.xlsx', sheet = 'Offensive')
 bio = read_excel('cyclonesFootball2019.xlsx', sheet = 'Biography')
 str(defense)
 
-as_numeric = function(x) as.numeric(x)
-new_offense +3
 
-bio %>%
-  separate(Height, into= c("ft","inches")) %>%
-  mutate(Height = as.numeric(ft)*12 +as.numeric(inches)) 
+
+
+defClean<- defense %>% 
+  mutate(Name =as.factor(Name)) %>%
+  mutate(Opponent_Opponent = as.factor(Opponent_Opponent)) %>%
+  mutate_at(vars(contains("Tackles")), funs(as.numeric)) %>%
+  mutate_at(vars(contains("Turnover")), funs(as.numeric)) %>%
+  mutate_at(vars(contains("Pass")), funs(as.numeric)) 
+
+
+bioClean<- bio %>% 
+  mutate(Name =as.factor(Name)) %>%
+  mutate_at(vars(contains("Weight")), funs(as.numeric))
+
+bioClean %>%
+  separate(Height, into = c("ft", "inches")) %>%
+  mutate(Height = as.numeric(ft)*12 + as.numeric(inches))
+bioClean[,ft] = bioClean[,Height]
+
+str(bioClean)

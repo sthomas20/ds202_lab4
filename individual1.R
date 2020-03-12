ccbabd5a456079b99168ee54041624e768d6d9c1
sthomas20 <sthomas8@iastate,edugit config --global user.email sthomas8@iastate.edu>
2020-03-12 15:28:10 -0500

    Part 1 Complete

--- individual1.R
+++ individual1.R
@@ -10,7 +10,7 @@ bio<- read_excel('cyclonesFootball2019.xlsx', sheet='Biography')
 
 #PART ONE: Cleaning Data 
 
-#Question 1 and 2
+#Question 1-3
 defClean<- defense %>% 
   mutate(Name =as.factor(Name)) %>%
   mutate(Opponent_Opponent = as.factor(Opponent_Opponent)) %>%
@@ -18,7 +18,7 @@ defClean<- defense %>%
   mutate_at(vars(contains("Turnover")), funs(as.numeric)) %>%
   mutate_at(vars(contains("Pass")), funs(as.numeric)) 
   
-str(defense)
+str(defClean)
 
 offClean<- offense %>% 
   mutate(Name =as.factor(Name)) %>%
@@ -27,15 +27,16 @@ offClean<- offense %>%
   mutate_at(vars(contains("Receiving")), funs(as.numeric)) %>%
   mutate_at(vars(contains("Passing")), funs(as.numeric)) 
  
-str(offense)
+str(offClean)
 
 bioClean<- bio %>% 
   mutate(Name =as.factor(Name)) %>%
   mutate_at(vars(contains("Weight")), funs(as.numeric)) %>%
   separate(Height, into = c("ft", "inches")) %>%
-  mutate(Height = as.numeric(ft)*12 + as.numeric(inches)) 
+  mutate(Height = as.numeric(ft)*12 + as.numeric(inches)) %>%
+  select(-c(ft, inches)) %>%
+  select(1:2, Height, everything())
   
 str(bioClean)
 
-#Question 3
 

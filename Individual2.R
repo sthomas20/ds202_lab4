0967f3ffaa107e8e93386fe2a667fbdc67d8aa16
John Nownes <jnownes@iastate.edu>
2020-03-12 15:57:23 -0500

    Finished 2.1 (Making defClean tidy)

--- Individual2.R
+++ Individual2.R
@@ -6,7 +6,6 @@ str(defense)
 
 
 
-
 defClean<- defense %>% 
   mutate(Name =as.factor(Name)) %>%
   mutate(Opponent_Opponent = as.factor(Opponent_Opponent)) %>%
@@ -14,14 +13,26 @@ defClean<- defense %>%
   mutate_at(vars(contains("Turnover")), funs(as.numeric)) %>%
   mutate_at(vars(contains("Pass")), funs(as.numeric)) 
 
+str(defClean)
+
+offClean<- offense %>% 
+  mutate(Name =as.factor(Name)) %>%
+  mutate(Opponent_Opponent = as.factor(Opponent_Opponent)) %>%
+  mutate_at(vars(contains("Rushing")), funs(as.numeric)) %>%
+  mutate_at(vars(contains("Receiving")), funs(as.numeric)) %>%
+  mutate_at(vars(contains("Passing")), funs(as.numeric)) 
+
+str(offClean)
 
 bioClean<- bio %>% 
   mutate(Name =as.factor(Name)) %>%
-  mutate_at(vars(contains("Weight")), funs(as.numeric))
-
-bioClean %>%
+  mutate_at(vars(contains("Weight")), funs(as.numeric)) %>%
   separate(Height, into = c("ft", "inches")) %>%
-  mutate(Height = as.numeric(ft)*12 + as.numeric(inches))
-bioClean[,ft] = bioClean[,Height]
+  mutate(Height = as.numeric(ft)*12 + as.numeric(inches)) %>%
+  select(-c(ft, inches)) %>%
+  select(1:2, Height, everything())
 
 str(bioClean)
+
+defClean %>%
+  pivot_longer(Tackles_Solo:Pass_PB, names_to = 'Statistic')
